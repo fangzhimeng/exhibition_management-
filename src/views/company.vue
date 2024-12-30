5e2584c999bbdcdaa8bbfb1d7c4ac48443da5276
root <admin@example.com>
2024-12-30 19:44:00 +0800

    基础页面

--- src/views/company.vue
+++ src/views/company.vue
@@ -18,96 +18,183 @@
     </avue-crud>
   </basic-container>
 
-  <div  class="table-container">
+  <div class="table-container">
 
-    <el-table ref="userTable" :header-cell-style="{ background: '#F2F6FC', color: '#131414' }" :data="dataList" border max-height="500">
-      <el-table-column label="标识ID" type="index" width="100" align="center" />
-      <el-table-column v-for="item in titleList" :key="item.value" :label="item.label" :prop="item.value" />
+    <el-table ref="userTable" :header-cell-style="{ background: '#F2F6FC', color: '#131414' }" :data="dataList" border
+              max-height="500">
+      <el-table-column label="标识ID" type="index" width="100" align="center"/>
+      <el-table-column v-for="item in titleList" :key="item.value" :label="item.label" :prop="item.value"/>
+      <!-- 操作列 -->
+      <el-table-column
+          label="操作"
+          width="150"
+          fixed="right">
+        <template #default="scope">
+          <el-button
+              size="small"
+              @click="handleView(scope.row)">
+            查看
+          </el-button>
+          <el-button
+              size="small"
+              type="primary"
+              @click="handleEdit(scope.row)">
+            编辑
+          </el-button>
+        </template>
+      </el-table-column>
     </el-table>
 
     <table>
-<!--      <tr v-for="item in staticData" :key="item.id">-->
-<!--        <td>{{ item.name }}</td>-->
-<!--        <td>{{ item.age }}</td>-->
-<!--      </tr>-->
+      <!--      <tr v-for="item in staticData" :key="item.id">-->
+      <!--        <td>{{ item.name }}</td>-->
+      <!--        <td>{{ item.age }}</td>-->
+      <!--      </tr>-->
     </table>
   </div>
 </template>
 <script>
 export default {
   data() {
-    2.4+2.4+2.2+1.9+1.9
-    2.6+2.6+2.4+2.1+2.1
-    2.8+2.8+2.6+2.3+2.3
-    3.0+3.0+2.6+2.3+2.3
-    3.2+3.2+2.8+2.5+2.5
-    24-12=12
     return {
       //数据List
-      dataList: [{exhibition_name: '张三',  phone_contacts_number: 20,  contact_situation_real_time_des: '男', light_ground_area: '男', outdoor_light_ground_area: '男', booth_no: '男', data_source_lead_salesperson: '男'
-        , phone_number_400: '男', website: '男', E_mail: '男', exhibition_modifier: '男',
+      dataList: [{
+        exhibition_name: '张三',
+        phone_contacts_number: 20,
+        contact_situation_real_time_des: '男',
+        light_ground_area: '男',
+        outdoor_light_ground_area: '男',
+        booth_no: '男',
+        data_source_lead_salesperson: '男'
+        ,
+        phone_number_400: '男',
+        website: '男',
+        E_mail: '男',
+        exhibition_modifier: '男',
 
-        update_time: '男', owner: '男', creator: '男', number: '男',data_classification_creation_time: '男', operate: '男'}],
+        update_time: '男',
+        owner: '男',
+        creator: '男',
+        number: '男',
+        data_classification_creation_time: '男',
+        operate: '男'
+      }],
       //列名List
       titleList: [ // 这里是静态数据的数组
-        { id: 1, label: '参展名称',value:'exhibition_name'},
-        { id: 2, label: '联系电话联络次数',value:'phone_contacts_number'},
-        { id: 3, label: '联络情况实时描述',value:'contact_situation_real_time_des'},
-        { id: 4, label: '光地面积',value:'light_ground_area'},
-        { id: 5, label: '室外光地面积',value:'outdoor_light_ground_area'},
-        { id: 6, label: '展位号',value:'booth_no'},
-        { id: 7, label: '数据来源主办业务员',value:'data_source_lead_salesperson'},
-        { id: 8, label: '400电话',value:'phone_number_400'},
+        {id: 1, label: '参展名称', value: 'exhibition_name'},
+        {id: 2, label: '联系电话联络次数', value: 'phone_contacts_number'},
+        {id: 3, label: '联络情况实时描述', value: 'contact_situation_real_time_des'},
+        {id: 4, label: '光地面积', value: 'light_ground_area'},
+        {id: 5, label: '室外光地面积', value: 'outdoor_light_ground_area'},
+        {id: 6, label: '展位号', value: 'booth_no'},
+        {id: 7, label: '数据来源主办业务员', value: 'data_source_lead_salesperson'},
+        {id: 8, label: '400电话', value: 'phone_number_400'},
 
 
-        { id: 9, label: '网址',value:'website'},
-        { id: 10, label: 'E_mail',value:'E_mail'},
-        { id: 11, label: '所属展会修改者',value:'exhibition_modifier'},
-        { id: 12, label: '更新时间',value:'update_time'},
-        { id: 13, label: '所有者',value:'owner'},
-        { id: 14, label: '创建者',value:'creator'},
-        { id: 15, label: '编号',value:'number'},
-        { id: 16, label: '数据分类创建时间',value:'data_classification_creation_time'},
-        { id: 17, label: '操作',value:'operate'}
+        {id: 9, label: '网址', value: 'website'},
+        {id: 10, label: 'E_mail', value: 'E_mail'},
+        {id: 11, label: '所属展会修改者', value: 'exhibition_modifier'},
+        {id: 12, label: '更新时间', value: 'update_time'},
+        {id: 13, label: '所有者', value: 'owner'},
+        {id: 14, label: '创建者', value: 'creator'},
+        {id: 15, label: '编号', value: 'number'},
+        {id: 16, label: '数据分类创建时间', value: 'data_classification_creation_time'},
+        {id: 17, label: '操作', value: 'operate'}
       ]
       //
       // tableColumns:['公司模块查询','申请日期' ,'申请人','申请-开票类型','申请-发票抬头','申请-单位地址','申请-税号','申请-电话号码','申请-开户银行'
       //   '申请-银行账号', '申请-发票金额','开票类型','发票抬头','开票单位','开票金额¥','开票金额$','发票号码(起)','发票号码(讫)','发票张数','快递单号',
-        //   '收件人','参展单位', '搭建单位','收费总额','已收款项','已开金额','展商模块查询','所有者','编号','创建时间','创建者','操作']
+      //   '收件人','参展单位', '搭建单位','收费总额','已收款项','已开金额','展商模块查询','所有者','编号','创建时间','创建者','操作']
     };
   },
   data2() {
     return {
 
       titleList2: [ // 这里是静态数据的数组
-        { id: 1, label: '参展名称',value:'exhibition_name'},
-        { id: 2, label: '联系电话联络次数',value:'phone_contacts_number'},
-        { id: 3, label: '联络情况实时描述',value:'contact_situation_real_time_des'},
-        { id: 4, label: '光地面积',value:'light_ground_area'},
-        { id: 5, label: '室外光地面积',value:'outdoor_light_ground_area'},
-        { id: 6, label: '展位号',value:'booth_no'},
-        { id: 7, label: '数据来源主办业务员',value:'data_source_lead_salesperson'},
-        { id: 8, label: '400电话',value:'phone_number_400'},
+        {id: 1, label: '参展名称', value: 'exhibition_name'},
+        {id: 2, label: '联系电话联络次数', value: 'phone_contacts_number'},
+        {id: 3, label: '联络情况实时描述', value: 'contact_situation_real_time_des'},
+        {id: 4, label: '光地面积', value: 'light_ground_area'},
+        {id: 5, label: '室外光地面积', value: 'outdoor_light_ground_area'},
+        {id: 6, label: '展位号', value: 'booth_no'},
+        {id: 7, label: '数据来源主办业务员', value: 'data_source_lead_salesperson'},
+        {id: 8, label: '400电话', value: 'phone_number_400'},
 
 
-        { id: 9, label: '网址',value:'website'},
-        { id: 10, label: 'E_mail',value:'E_mail'},
-        { id: 11, label: '所属展会修改者',value:'exhibition_modifier'},
-        { id: 12, label: '更新时间',value:'update_time'},
-        { id: 13, label: '所有者',value:'owner'},
-        { id: 14, label: '创建者',value:'creator'},
-        { id: 15, label: '编号',value:'number'},
-        { id: 16, label: '数据分类创建时间',value:'data_classification_creation_time'},
-        { id: 17, label: '操作',value:'operate'}
+        {id: 9, label: '网址', value: 'website'},
+        {id: 10, label: 'E_mail', value: 'E_mail'},
+        {id: 11, label: '所属展会修改者', value: 'exhibition_modifier'},
+        {id: 12, label: '更新时间', value: 'update_time'},
+        {id: 13, label: '所有者', value: 'owner'},
+        {id: 14, label: '创建者', value: 'creator'},
+        {id: 15, label: '编号', value: 'number'},
+        {id: 16, label: '数据分类创建时间', value: 'data_classification_creation_time'},
+        {id: 17, label: '操作', value: 'operate'}
       ]
     };
-  }
+  },
+  setup() {
+    // ... 之前的代码保持不变
 
+    // 添加表格操作方法
+    const handleView = (row) => {
+      // 查看详情
+      router.push({
+        path: `/corporation/detail/${row.id}`
+      })
+    }
+
+    const handleEdit = (row) => {
+      // 编辑
+      router.push({
+        path: `/corporation/edit/${row.id}`
+      })
+    }
+
+    const handleSelectionChange = (selection) => {
+      // 处理多选
+      selectedRows.value = selection
+    }
+
+    return {
+      // ... 之前的返回值
+      handleView,
+      handleEdit,
+      handleSelectionChange
+    }
+  }
 }
 </script>
 <style scoped>
-.table-container {
-  overflow-y: scroll; /* 创建垂直滚动条 */
-  max-height: 400px; /* 根据需求设定最大高度 */
-}
+
+
+  .table-container {
+    overflow-y: scroll; /* 创建垂直滚动条 */
+    max-height: 400px; /* 根据需求设定最大高度 */
+  }
+
+  /*... 之前的样式保持不变*/
+
+  /*添加表格相关样式*/
+
+
+    /*表头样式*/
+  ::v-deep  .el-table__header-wrapper th {
+      background-color: #f5f7fa;
+      color: #606266;
+      font-weight: 500;
+      text-align: center;
+    }
+
+
+    /*单元格样式*/
+  ::v-deep .el-table__body-wrapper td {
+      padding: 8px 0;
+    }
+
+    /*悬浮提示样式优化*/
+  ::v-deep  .el-tooltip__popper {
+      max-width: 50%;
+    }
+
 </style>
